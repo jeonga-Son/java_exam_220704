726d3487f98b4f89d7c714e2138c69bb470da4b8
jeonga-Son <jjeong0106@gmail.com>
2022-07-05 15:54:12 +0900

    곱하기 기능 구현

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -5,30 +5,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
-    @Test
+    @Test //컴퓨터가 읽는 주석
     public void 더하기_1() {
-        int rs = Calculator.run("10 + 20");
+        int rs = Calculator.calc("10 + 20");
 
         assertEquals(30, rs);
     }
     @Test
     public void 더하기_2() {
-        int rs = Calculator.run("20 + 20");
+        int rs = Calculator.calc("20 + 20");
 
         assertEquals(40, rs);
     }
 
     @Test
     public void 더하기_3() {
-        int rs = Calculator.run("10 + 10");
+        int rs = Calculator.calc("10 + 10");
 
         assertEquals(20, rs);
     }
 
     @Test
     public void 빼기_1() {
-        int rs = Calculator.run("10 - 10");
+        int rs = Calculator.calc("10 - 10");
 
         assertEquals(0, rs);
+    }
+
+    @Test
+    public void 곱하기_1() {
+        int rs = Calculator.calc("10 * 10");
+
+        assertEquals(100, rs);
     }
 }
