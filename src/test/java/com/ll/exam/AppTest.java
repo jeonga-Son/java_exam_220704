f6d818b056dd446436277effd2070b2612acb0d0
jeonga-Son <jjeong0106@gmail.com>
2022-07-05 12:25:36 +0900

    함수명을 run으로 수정

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -6,22 +6,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    public void 더하기_테스트() {
-        int rs = Calculator.add("10 + 20");
+    public void 더하기_1() {
+        int rs = Calculator.run("10 + 20");
 
         assertEquals(30, rs);
     }
     @Test
-    public void 더하기_테스트2() {
-        int rs = Calculator.add("20 + 20");
+    public void 더하기_2() {
+        int rs = Calculator.run("20 + 20");
 
         assertEquals(40, rs);
     }
 
     @Test
-    public void 더하기_테스트3() {
-        int rs = Calculator.add("10 + 10");
+    public void 더하기_3() {
+        int rs = Calculator.run("10 + 10");
 
         assertEquals(20, rs);
     }
+
+
 }
